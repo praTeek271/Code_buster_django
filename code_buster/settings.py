31e2f6603a0e458b298cf06acc9aeb8c3bbe54fb
praTeek271 <prateekpatriotasme@gmail.com>
2022-08-27 05:31:39 +0530

    ver1:patch(reg)

--- code_buster/settings.py
+++ code_buster/settings.py
@@ -135,6 +135,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
     ]
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
