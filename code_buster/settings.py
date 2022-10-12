7683557d642f4918552ae7c90cf29475fa1a807e
praTeek271 <prateekpatriotasme@gmail.com>
2022-10-13 03:49:00 +0530

    DEBUG_fALSE

--- code_buster/settings.py
+++ code_buster/settings.py
@@ -34,8 +34,8 @@ with open('code_buster\.env') as file:
 # SECRET_KEY = 'django-insecure-ed!4!u7wa4+90fivg_bqmqi(ii+sdfps7itml=ultto@$a9&i+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
